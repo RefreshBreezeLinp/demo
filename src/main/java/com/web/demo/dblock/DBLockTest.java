f3c8600313c8cb3930f92933fb6ece1d24c1316d
linp-MHC <changkong@maihaoche.com>
2018-07-16 17:02:49 +0800

    新增数据库锁讲解

---
+++ src/main/java/com/web/demo/dblock/DBLockTest.java
@@ -0,0 +1,15 @@
+package com.web.demo.dblock;
+
+public class DBLockTest {
+
+    //数据库锁：https://blog.csdn.net/samjustin1/article/details/52210125
+    //锁分类：
+    //  1. 共享锁
+    //  2. 更新锁
+    //  3. 拍他锁
+    //  4. 意向锁
+    //  5. 计划锁
+    public static void main(String[] args) {
+
+    }
+}
