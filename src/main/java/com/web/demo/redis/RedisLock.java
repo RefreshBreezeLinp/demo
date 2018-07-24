a43404290d8d72a7896445e4790b9389be3b4435
RefreshBreezeLinp <18720913112@163.com>
2018-07-24 17:17:27 +0800

    新增redis实现分布式锁blog地址

--- src/main/java/com/web/demo/redis/RedisLock.java
+++ src/main/java/com/web/demo/redis/RedisLock.java
@@ -1,6 +1,7 @@
 package com.web.demo.redis;
 
 //https://github.com/yangliu0/DistributedLock redis 分布式锁 zookeeper 分布式锁
+//redis实现分布式锁讲解blog地址：https://www.cnblogs.com/liuyang0/p/6744076.html
 public class RedisLock extends Thread {
 
     private Service service;
