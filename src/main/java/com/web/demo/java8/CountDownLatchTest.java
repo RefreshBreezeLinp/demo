af1da13478b07b188d9c4772c85c3781b2b8ee95
xuzhu <xuzhu@myweimai.com>
2019-07-18 16:27:14 +0800

    countdownlatch源码解读

--- src/main/java/com/web/demo/java8/CountDownLatchTest.java
+++ src/main/java/com/web/demo/java8/CountDownLatchTest.java
@@ -26,6 +26,11 @@ public class CountDownLatchTest {
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("run2...");
                 countDownLatch.countDown();
             }
@@ -33,8 +38,11 @@ public class CountDownLatchTest {
 
         t2.start();
         t1.start();
+        System.out.println("main run...");
         countDownLatch.await();
         System.out.println("end");
     }
+
+    //next CyclicBarrier AQS
 
 }
